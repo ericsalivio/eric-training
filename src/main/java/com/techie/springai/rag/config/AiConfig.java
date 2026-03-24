442800e586feae989d583e5ab83c5c6b9e6e7303
eric salivio <ericsalivio.ccs@gmail.com>
2026-03-25 00:04:10 +0800

    test

--- src/main/java/com/techie/springai/rag/config/AiConfig.java
+++ src/main/java/com/techie/springai/rag/config/AiConfig.java
@@ -1,11 +1,15 @@
 package com.techie.springai.rag.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AiConfig {
+
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder) {
